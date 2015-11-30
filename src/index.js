affe003d24d45b8f59b033fab3fbfd073c0742e1
Les Orchard <me@lmorchard.com>
2015-11-30 18:17:53 -0500

    Starting to play with Outline-shaped components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const OutlineNode = React.createClass({
+  render() {
+    var children = (!this.props.node.children) ? '' :
+      (<Outline nodes={this.props.node.children} />);
+    return (
+      <li class="outline-node">
+        <span class="title">{this.props.node.title}</span>
+        {children}
+      </li>
+    );
+  }
+});
+
+const Outline = React.createClass({
+  render() {
+    var nodes = this.props.nodes.map(node => (<OutlineNode node={node} />));
+    return (
+      <ul class="outline">
+        {nodes}
+      </ul>
+    );
+  }
+});
+
+const App = React.createClass({
+  getInitialState() {
+    return {
+      nodes: [
+        {title: "alpha"},
+        {title: "beta", children: [
+          {title: "foo"},
+          {title: "bar"},
+          {title: "baz"}
+        ]},
+        {title: "gamma"}
+      ]
+    }
+  },
+  render() {
+    return (
+      <Outline nodes={this.state.nodes} />
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
+  <App />,
+  document.getElementById('app')
 );
