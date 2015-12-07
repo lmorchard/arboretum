ef92f271431ef51b95c7933a089afe174b089a2b
Les Orchard <me@lmorchard.com>
2015-12-06 19:31:42 -0500

    Add getNextNodePath and getPreviousNodePath util functions

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,62 @@
-export function foo() {
-  return true;
+export function splitPath (path) {
+  return path.split('.');
+}
+
+export function getNodeContext (state, path) {
+  var key = splitPath(path);
+  return { key,
+    parentKey: key.slice(0, -2),
+    index: parseInt(key[key.length - 1], 10),
+    value: state.getIn(key),
+    siblings: state.getIn(key.slice(0, -1))
+  }
+}
+
+export function getPreviousNodePath (state, path) {
+  var key = splitPath(path);
+  var index = parseInt(key.pop(), 10);
+  var siblings = key.length == 0 ? state : state.getIn(key);
+
+  // Pop up to the parent if past the first sibling.
+  if (index - 1 < 0) {
+    return (key.length == 0) ? null :
+      key.slice(0, -1).join('.');
+  }
+
+  // Step into previous sibling, or the deepest last child.
+  var step = key.concat([index - 1]);
+  while (state.hasIn(step.concat(['children']))) {
+    var children = state.getIn(step.concat(['children']));
+    step = step.concat(['children', String(children.size - 1)]);
+  }
+
+  return step.join('.');
+}
+
+export function getNextNodePath (state, path) {
+  var key = splitPath(path);
+
+  // Walk down into children
+  if (state.hasIn(key.concat(['children']))) {
+    return key.join('.') + '.children.0';
+  }
+
+  // Walk forward through siblings
+  var index = parseInt(key.pop(), 10);
+  var siblings = key.length == 0 ? state : state.getIn(key);
+  if (index + 1 < siblings.size) {
+    return key.concat([index + 1]).join('.');
+  }
+
+  // Walk up into parent's siblings
+  while (key.length) {
+    var _ = key.pop(); // 'children'
+    var parentIndex = parseInt(key.pop(), 10);
+    var parentSiblings = state.getIn(key);
+    if (parentIndex + 1 < parentSiblings.size) {
+      return key.concat([parentIndex + 1]).join('.');
+    }
+  }
+
+  return null;
 }
--- test/test-utils.js
+++ test/test-utils.js
@@ -1,12 +1,102 @@
 import { expect } from "chai";
 import { createStore, applyMiddleware } from 'redux';
-import { foo } from '../src/lib/utils';
+import Immutable from 'immutable';
 
-describe('utils', () => {
+import reducers from '../src/lib/reducers';
+import * as Utils from '../src/lib/utils';
 
-  describe('play', () => {
-    it('foo should return true', () => {
-      expect(foo()).to.equal(true);
+describe('utils', function () {
+
+  beforeEach(function () {
+
+    this.state = Immutable.fromJS([
+      {title: "alpha"},
+      {title: "beta", children: [
+        {title: "foo"},
+        {title: "bar", children: [
+          {title: "quux"},
+          {title: "xyzzy"}
+        ]},
+        {title: "baz"}
+      ]},
+      {title: "gamma"},
+      {title: "level1", children: [
+        {title: 'level2', children: [
+          {title: 'level3', children: [
+            {title: 'level4'}
+          ]}
+        ]}
+      ]},
+      {title: 'thud'}
+    ]);
+
+    this.store = createStore(reducers, {
+      nodes: this.state
+    });
+
+    this.expectedSeries = [
+      '0',
+      '1',
+      '1.children.0',
+      '1.children.1',
+      '1.children.1.children.0',
+      '1.children.1.children.1',
+      '1.children.2',
+      '2',
+      '3',
+      '3.children.0',
+      '3.children.0.children.0',
+      '3.children.0.children.0.children.0',
+      '4'
+    ];
+
+  });
+
+  describe('splitPath', function () {
+    it('should split a dot-delimited path into a key array', function () {
+      expect(Utils.splitPath('1.children.1.children.2')).to.deep.equal(['1', 'children', '1', 'children', '2']);
+    })
+  })
+
+  describe('getNodeContext', function () {
+    it('should construct contextual information for a node path', function () {
+      var path = '1.children.1.children.0';
+      var expected = {
+        key: ['1', 'children', '1', 'children', '0'],
+        parentKey: ['1', 'children', '1'],
+        index: 0,
+        value: this.state.getIn(['1', 'children', '1', 'children', '0']),
+        siblings: this.state.getIn(['1', 'children', '1', 'children'])
+      };
+      var result = Utils.getNodeContext(this.state, path);
+      expect(result).to.deep.equal(expected);
+    })
+  })
+
+  describe('getNextNodePath', function () {
+    it('should find the path to the next node', function () {
+      var current, expected, result;
+      current = this.expectedSeries.shift();
+      while (this.expectedSeries.length) {
+        expected = this.expectedSeries.shift();
+        result = Utils.getNextNodePath(this.state, current);
+        expect(result).to.equal(expected);
+        current = expected;
+      }
+    })
+  });
+
+  describe('getPreviousNodePath', function () {
+    it('should find the path to the previous node', function () {
+      var current, expected, result;
+      this.expectedSeries.reverse();
+      current = this.expectedSeries.shift();
+      while (this.expectedSeries.length) {
+        expected = this.expectedSeries.shift();
+        result = Utils.getPreviousNodePath(this.state, current);
+        expect(result).to.equal(expected);
+        current = expected;
+      }
     })
   });
 
