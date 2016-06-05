1b49e8c497beee5fefc037b31a65af010de616c3
Les Orchard <me@lmorchard.com>
2016-06-05 18:51:57 -0400

    Tweaks to not try saving when saveFilename is empty, refresh file list on save

--- src/lib/components/NavBar.js
+++ src/lib/components/NavBar.js
@@ -5,8 +5,6 @@ import * as storages from '../storages';
 import stringify from 'json-stringify-pretty-compact';
 import classnames from 'classnames';
 
-const filename = 'foo.json';
-
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
@@ -18,6 +16,7 @@ class NavBar extends React.Component {
       saveFilename: ''
     };
   }
+
   render() {
     const {dispatch, meta, nodes, storage} = this.props;
     const {loading, filelist, saveFilename, loadFilename} = this.state;
@@ -27,6 +26,7 @@ class NavBar extends React.Component {
           {storage &&
             <span>
               <button onClick={ev => this.newDoc(ev)}>New</button>
+
               <select style={{ width: '15em' }} value={loadFilename}
                       onChange={(ev) => this.setState({loadFilename: ev.target.value})}>
                 {filelist.map((filename, idx) => (
@@ -35,18 +35,18 @@ class NavBar extends React.Component {
               </select>
               <button onClick={ev => this.refreshList(ev)}>&#8635;</button>
               <button onClick={ev => this.load(ev)}>Load</button>
+
               <input type="text" style={{width: '15em'}} value={saveFilename}
                      onChange={(ev) => this.setState({saveFilename: ev.target.value})} />
               <button onClick={ev => this.save(ev)}>Save</button>
+
             </span>}
         </div>
         <div className="settings">
           {!storage &&
             <button onClick={ev => this.connectDropbox(ev)}>Connect to Dropbox</button>}
           {storage && storage.name === 'DropboxStorage' &&
-            <span>
-              <span>Dropbox: {storage.account.email}</span>
-            </span>}
+            <span>Dropbox: {storage.account.email}</span>}
           {storage &&
             <button onClick={ev => this.disconnect(ev)}>Disconnect</button>}
         </div>
@@ -80,6 +80,7 @@ class NavBar extends React.Component {
 
   newDoc(ev) {
     const {dispatch, nodes, storage} = this.props;
+    this.setState({saveFilename: ''});
     dispatch(loadNodes([
       {title: 'Click here to edit.'}
     ]));
@@ -88,6 +89,7 @@ class NavBar extends React.Component {
   save(ev) {
     const {nodes, storage} = this.props;
     const {saveFilename} = this.state;
+    if (!saveFilename) { return; }
     const data = stringify(nodes.toJS());
     this.setState({loading: true});
     storage.put(saveFilename, data).then(result => {
@@ -95,6 +97,7 @@ class NavBar extends React.Component {
         loading: false,
         loadFilename: saveFilename
       });
+      this.refreshList(ev);
     });
   }
 
